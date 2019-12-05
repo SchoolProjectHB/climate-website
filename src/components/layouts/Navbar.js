d22a63df6ad7d41b2749ad9873b063f9c538f65b
Lisa Jernaeus <lisa@Lisas-MacBook-Pro-3.local>
2019-12-05 12:19:57 +0100

    change logo link

--- src/components/layouts/Navbar.js
+++ src/components/layouts/Navbar.js
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 function Navbar() {
     return (
 <nav className="navbar navbar-expand-lg navbar-light bg-light">
-  <Link className="navbar-brand ml-5" to="/klimatforandringar">
+  <Link className="navbar-brand ml-5" to="/">
   <img src={logo} alt="logo" style={{ width: '90px' }}/>
   </Link>
 
