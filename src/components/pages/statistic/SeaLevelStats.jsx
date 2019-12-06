7763baef0c1d66bde43968f0ffbab87e0f41bfdd
System Administrator <root@Eminas-MacBook-Air-2.local>
2019-12-06 11:12:50 +0100

    inital commit global temp

--- src/components/pages/statistic/SeaLevelStats.jsx
+++ src/components/pages/statistic/SeaLevelStats.jsx
@@ -13,24 +13,20 @@ class MyBarChartSeaLevel extends Component {
 let co2 = this.props.CO2Emission; 
 if(co2 === undefined) return <p>There is no data </p>;
 
-    const dataChartSeaLevel = [
-        {name: 'Mean', value: co2["Mean"], uv: co2["Mean"]},
-    ];
-
 //const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042'];
 
         return (  
           <React.Fragment>
 
             
-<LineChart width={600} height={300} data={dataChartSeaLevel}
+<LineChart width={600} height={300} data={co2}
             margin={{top: 5, right: 30, left: 20, bottom: 5}}>
-       <XAxis dataKey="name"/>
+       <XAxis dataKey="Time"/>
        <YAxis/>
        <CartesianGrid strokeDasharray="3 3"/>
        <Tooltip/>
        <Legend />
-       <Line type="monotone" dataKey="value" stroke="#8884d8" activeDot={{r: 8}}/>
+       <Line type="monotone" dataKey="GMSL" stroke="#8884d8" activeDot={{r: 8}}/>
        
        
       </LineChart>
