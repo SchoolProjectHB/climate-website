b03674befc27d8dfc0a3b67041f01d4f74b712bf
Lisa Jernaeus <lisa@Lisas-MacBook-Pro-3.local>
2019-12-11 14:19:40 +0100

    changes

--- src/components/pages/Start.js
+++ src/components/pages/Start.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { Button } from "semantic-ui-react";
+import startsida from "../components/startsida.png";
 
 class Start extends Component {
     render() {
@@ -7,6 +8,8 @@ class Start extends Component {
       <button class="huge ui button">
         Knapp
       </button>
+
+
 
       );
     }
