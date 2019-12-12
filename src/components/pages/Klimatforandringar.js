6df9c1628e3801b240eeb934097e34cc3b4cd8ae
System Administrator <root@Eminas-MacBook-Air-2.local>
2019-12-12 10:47:07 +0100

    intial commit added buttons

--- src/components/pages/Klimatforandringar.js
+++ src/components/pages/Klimatforandringar.js
@@ -1,58 +1,72 @@
-import React, { Fragment, Component } from 'react';
+import React, { Component } from 'react';
 import 'semantic-ui-css/semantic.min.css'
-import ReactDOM from "react-dom";
-import AngryJoe from '../layouts/AngryJoe'
-import * as statistic from './statistic';
-class Klimatforandringar extends Component {
-    state = {
-      selectedCardType: ''
-    };
-  
-  
-    render(){
-      return (
-        <Fragment>
-       
-        <section className="app-section container">
-          {this.renderCardSelector()}
+//import ReactDOM from "react-dom";
+//import AngryJoe from '../layouts/AngryJoe'
+//import * as statistic from './statistic';
 
-          <div className="top-margin-small">
-            {this.renderSelectedCard(this.state.selectedCardType)}
-          </div>
-        </section>
-      </Fragment>
+class Klimatforandringar extends Component { 
+  render() {
+    return (
+      <div class="Start">  
+        <a href="/FossilFuels"><button class="small ui button">Fossila Bränslen</button></a>
+        <a href="/GlobalTemp"><button class="small ui button">Globala Temperaturer</button></a>
+        <a href="/Glacier"><button class="small ui button">Glaciärer</button></a>
+        <a href="/SeaLevel"><button class="small ui button">Havsnivåer</button></a>
+        </div>
     );
   }
-
-    renderCardSelector() {
-      return (
-        
-          <select className="ui dropdown"onChange={(e) => this.setState({ selectedCardType: e.target.value })}>
-         <input type="active" name="Kategori">
-           </input>
-           <option value="0">Välj en kategori</option>
-            <option value="CardA">Fossila Bränslen</option>
-            <option value="CardB">Globala Temperaturer</option>
-            <option value="CardC">Glaciärer</option>
-            <option value="CardD">Havsnivåer</option>
-          </select>
-      );
-    }
-
-    renderSelectedCard(selectedCardType) {
-        if (!selectedCardType)
-          return <AngryJoe text="Välj en kategorii" />;
-    if(selectedCardType==="0")
-    return; 
-        const Card = statistic[selectedCardType];
-    
-        return <Card />;
-      }
-
 }
+
+//class Klimatforandringar extends Component {
+  //  state = {
+    //  selectedCardType: ''
+    //};
+  
+  
+    //render(){
+      //return (
+        //<Fragment>
+       
+      //  <section className="app-section container">
+        //  {this.renderCardSelector()}
+
+          //<div className="top-margin-small">
+            //{this.renderSelectedCard(this.state.selectedCardType)}
+          //</div>
+        //</section>
+      //</Fragment>
+    //);
+  //}
+  
+    //renderCardSelector() {
+      //return (
+        
+          //<select className="ui dropdown"onChange={(e) => this.setState({ selectedCardType: e.target.value })}>
+         //<input type="active" name="Kategori">
+          // </input>
+           //<option value="0">Välj en kategori</option>
+            //<option value="CardA">Fossila Bränslen</option>
+            //<option value="CardB">Globala Temperaturer</option>
+            //<option value="CardC">Glaciärer</option>
+            //<option value="CardD">Havsnivåer</option>
+          //</select>
+      //);
+    //}
+
+    //renderSelectedCard(selectedCardType) {
+      //  if (!selectedCardType)
+        //  return <AngryJoe text="Välj en kategorii" />;
+    //if(selectedCardType==="0")
+    //return; 
+      //  const Card = statistic[selectedCardType];
+    
+   //     return <Card />;
+     // }
+
+//}
   
  
-const rootElement = document.getElementById("root");
-ReactDOM.render(<Klimatforandringar />, rootElement);
+//const rootElement = document.getElementById("root");
+//ReactDOM.render(<Klimatforandringar />, rootElement);
 
 export default Klimatforandringar;
