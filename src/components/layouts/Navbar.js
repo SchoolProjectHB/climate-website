a6fc784c68ca9e2a10c18821e6ab2393fef2deb4
klauiajasionek <klaudiajasionek@hotmail.com>
2019-12-16 11:15:35 +0100

    commit changes navbar

--- src/components/layouts/Navbar.js
+++ src/components/layouts/Navbar.js
@@ -6,21 +6,24 @@ function Navbar() {
     return (
 <nav className="navbar navbar-expand-lg navbar-light bg-white">
   <Link className="navbar-brand ml-5" to="/">
-  <img src={logo} alt="logo" style={{ width: '90px' }}/>
+  <img src={logo} class="logo" alt="logo" style={{ width: '90px' }}/>
+ 
   <span className="sr-only">(current)</span></Link>
-  <div className="collapse navbar-collapse" id="navbarSupportedContent">
+   <div className="collapse navbar-collapse" id="navbarSupportedContent">
     <ul className="navbar-nav mr-auto">
-      <li className="nav-item">
+      {/* <li className="nav-item">
         <Link className="nav-link text-black text-uppercase ml-5" to="/klimatforandringar">Klimatförändringar</Link>
       </li>
       <li className="nav-item">
         <Link className="nav-link text-black text-uppercase ml-5" to="/ordlista">Ordlista</Link>
       </li>
-      {/* <li className="nav-item">
+      <li className="nav-item">
         <Link className="nav-link text-black text-uppercase ml-5" to="/omoss">Om oss</Link>
-      </li> */}
-    </ul>
-  </div>
+      </li> 
+    </ul>*/}
+ 
+</ul>
+ </div>
 </nav>
     );
 }
