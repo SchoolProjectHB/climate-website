4c6aa72321753507c26adc5af99fb1440a98078e
Lisa Jernaeus <lisa@Lisas-MacBook-Pro-3.local>
2020-01-08 11:35:12 +0100

    changes

--- src/components/pages/statistic/FossilFuels.jsx
+++ src/components/pages/statistic/FossilFuels.jsx
@@ -4,7 +4,6 @@ import "semantic-ui-css/semantic.min.css";
 import fossilimage from "./fossilimage.png";
 import siggefossil from "./SiggeFossilaBranslen.png";
 
-
 // import Ordlista from '../components/pages/Ordlista';
 
 class App extends Component {
@@ -30,53 +29,57 @@ class App extends Component {
                 <h4>Fossila utsläpp</h4>
                 <p>
                   Bensin, diesel, olja, naturgas och kol är exempel på fossila
-                  bränslen. Dessa fossila bränslena används till fordon, kol och
-                  oljekraftverk. När fossila bränslen används, släpper de ut
-                  gaser som skadar luften. 
+                  bränslen. Dessa används till fordon, kol och oljekraftverk.
+                  När fossila bränslen används släpper de ut gaser som skadar
+                  luften.
                   <br></br>
                   <br></br>
-                  Några fossila bränslen är:
-                  svaveldioxid, kväveoxider och koldioxid (CO2). Alla dessa
-                  bränslen bidrar till den globala uppvärmningen.
+                  Några fossila bränslen är svaveldioxid, kväveoxider och
+                  koldioxid (CO2). Alla dessa bränslen bidrar till den globala
+                  uppvärmningen.
                 </p>
 
-                <img src={fossilimage} class="fossilimage" alt="clothes hanging to dry"></img>
-
+                <img
+                  src={fossilimage}
+                  class="fossilimage"
+                  alt="clothes hanging to dry"
+                ></img>
               </div>
             </div>
 
             <div className="box col-sm">
               <h4>Vad är fossila bränslen?</h4>
+              <b>Gasbränsle:</b>
               <p>
-                Gasbränsle: Gasbränslen är ett samlingsnamn för bränslen som är
-                i gasform. Exempel på gasbränslen är: biogas, naturgas, gasol
-                och vätgas. 
+                Gasbränslen är bränslen som är i gasform. Exempel på gasbränslen
+                är biogas, naturgas, gasol och vätgas.
               </p>
-                <p> 
-                Flytande bränsle: Flytande bränsle är en vätska som
-                innehåller mycket energi. Exempel på flytande bränslen är:
-                bensin, diesel, etanol och biodiesel. 
-                </p>
-                <p> 
-
-                Fasta bränslen: Fast
-                bränsle är olika former av fast material som kan brännas för att
-                frigöra energi, vilket ger värme och ljus genom
-                förbränningsprocessen. Vanliga exempel på fasta bränslen är trä,
-                kol och träpellets. 
-                </p>
-                <p> 
-                Cement: Cement är det som används för att
-                bygga byggnader. Cementindustrin är en av de två största
-                producenterna av koldioxid (CO 2 ) och skapar upp till 8% av de
-                globala konstgjorda utsläppen av denna gas, varav 50% kommer
-                från den kemiska processen och 40% från brinnande bränsle.
+              <b>Flytande bränsle:</b>
+              <p>
+                {" "}
+                Flytande bränsle är en vätska som innehåller mycket energi.
+                Exempel på flytande bränslen är bensin, diesel och etanol.
+              </p>
+              <b>Fasta bränslen:</b>
+              <p>
+                {" "}
+                Fasta bränslen är olika former av fast material som kan brännas.
+                Detta kan till exempel vara trä, kol eller träpellets.
+              </p>
+              <b>Cement:</b>{" "}
+              <p>
+                {" "}
+                Cement används bland annat för att bygga byggnader.
+                Cementindustrin bidrar till stora utsläpp av koldioxid (CO2).
               </p>
             </div>
 
             <div className="boxsigge col-sm">
-            <img src={siggefossil} class="siggefossil" alt="sigge info."></img>
-              
+              <img
+                src={siggefossil}
+                class="siggefossil"
+                alt="sigge info."
+              ></img>
             </div>
           </div>
           <div className="row">
@@ -84,16 +87,14 @@ class App extends Component {
               <div className="App">
                 <h1>Statistik över fossila utsläpp</h1>
 
-         <p className="descriptionCo2">co2</p>
+                <p className="descriptionCo2">co2</p>
 
                 <MyBarChart
                   CO2Emission={this.state.CO2Emission}
                   onYearFilter={this.handleYearFilter}
                 ></MyBarChart>
 
-                      <p className="descriptionYear">År</p>
-                     
-
+                <p className="descriptionYear">År</p>
               </div>
             </div>
           </div>
