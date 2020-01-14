3dbde959b018ae5110e976c124118a5393bba04d
Lisa Jernaeus <lisa@lisas-mbp-3.customer.ica.local>
2020-01-14 21:30:36 +0100

    changes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Navbar from './components/layouts/Navbar'; 
-import Footer from './components/layouts/Footer'; 
+import Navbar from './components/layouts/Navbar';
+import Footer from './components/layouts/Footer';
 // React Router Import
-import { Switch, Route, Redirect } from 'react-router-dom'; 
+import { Switch, Route, Redirect } from 'react-router-dom';
 // Import pages
 import Start from './components/pages/Start';
 import Klimatforandringar from './components/pages/Klimatforandringar';
-import Ordlista from './components/pages/Ordlista';
-import Omoss from './components/pages/Omoss';
+
 
 
 
@@ -23,8 +22,6 @@ class App extends Component {
           <Redirect from= "/" exact to = "/start"></Redirect>
           <Route path="/start" component={Start}/>
           <Route path="/statistic" component={Klimatforandringar}/>
-          <Route path="/ordlista" component={Ordlista}/>
-          <Route path="/omoss" component={Omoss}/>
         </Switch>
         <Footer />
 
